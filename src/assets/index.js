4dd2221c4f983e968e4fbf526d843e90faf51540
구혁모 <gurah129@gmail.com>
2026-08-13 15:54:18 +0900

    🍱 분석 결과 및 추천 쪽 에셋 추가

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,8 @@
 // 아이콘 및 이미지 파일들을 한 곳에서 관리하기 위한 인덱스 파일(Barrel export)
 // 이미지 추가하면 여기서 임포트하고 내보내기
-// import logoWhite from '@/assets/logo-white.svg';
+import mealPlaceholder from './meal-result/meal-placeholder.jpeg';
+import perfectDecoration from './meal-recommendation/perfect-decoration.svg';
+import resultMascot from './meal-result/result-mascot.svg';
 
 // 임포트한 이미지 및 아이콘 내보내기
-export {};
+export { mealPlaceholder, perfectDecoration, resultMascot };
