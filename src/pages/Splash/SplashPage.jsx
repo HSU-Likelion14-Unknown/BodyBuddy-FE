575989cf5f00af501550712c0674c5d26be01747
huieunkim-dev <huieunkim.dev@gmail.com>
2026-08-15 19:49:49 +0900

    ⏪️ 스플래시 대기 시간 3초로 복원
    #9

--- src/pages/Splash/SplashPage.jsx
+++ src/pages/Splash/SplashPage.jsx
@@ -19,7 +19,7 @@ export default function SplashPage() {
     // 온보딩 여부 확인
     const timer = setTimeout(() => {
       navigate(hasOnboarded ? '/home' : '/onboarding/1');
-    }, 30000000); // 일단 임시적으로 3초로 지정
+    }, 3000);
 
     return () => clearTimeout(timer);
   }, [navigate]);
