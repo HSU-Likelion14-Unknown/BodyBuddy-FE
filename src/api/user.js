46ad955a162caef9677e37a3ffc65195a9671a91
huieunkim-dev <huieunkim.dev@gmail.com>
2026-08-19 23:52:58 +0900

    ✨ 익명 인증, 온보딩 API 모듈 생성
    #18

---
+++ src/api/user.js
@@ -0,0 +1,42 @@
+import api from './instance';
+
+const ALLERGEN_CODE_MAP = {
+  egg: 'EGG',
+  milk: 'MILK',
+  nuts: 'NUTS',
+  shellfish: 'SHELLFISH',
+  soy: 'SOY',
+  wheat: 'WHEAT',
+};
+
+const GENDER_MAP = {
+  male: 'MALE',
+  female: 'FEMALE',
+  none: 'PREFER_NOT_TO_SAY',
+};
+
+export async function putOnboarding({
+  nickname,
+  birthYear,
+  gender,
+  allergens,
+  customAllergens,
+  dislikedFoods,
+}) {
+  const mappedCodes = allergens
+    .filter((k) => k !== 'none')
+    .map((k) => ALLERGEN_CODE_MAP[k])
+    .filter(Boolean);
+
+  const allergyCodes = [...mappedCodes, ...customAllergens];
+
+  const res = await api.put('/users/me/onboarding', {
+    nickname,
+    birthYear: birthYear ?? null,
+    gender: GENDER_MAP[gender] ?? 'PREFER_NOT_TO_SAY',
+    allergyCodes: allergyCodes.length ? allergyCodes : [],
+    dislikedFoods: dislikedFoods.length ? dislikedFoods : [],
+  });
+
+  return res.data.data;
+}
