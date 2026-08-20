4f98bd8468ea777485cbca397befab200be13510
huieunkim-dev <huieunkim.dev@gmail.com>
2026-08-20 13:58:27 +0900

    ⏪️ 알레르기 비선호 빈 배열로 변경
    #20

--- src/api/user.js
+++ src/api/user.js
@@ -39,8 +39,8 @@ export async function putOnboarding({
     nickname,
     birthYear: birthYear ?? null,
     gender: GENDER_MAP[gender] ?? 'PREFER_NOT_TO_SAY',
-    allergyCodes: allergyCodes.length ? allergyCodes : ['NONE'],
-    dislikedFoods: dislikedFoods.length ? dislikedFoods : ['NONE'],
+    allergyCodes: allergyCodes.length ? allergyCodes : [],
+    dislikedFoods: dislikedFoods.length ? dislikedFoods : [],
   });
 
   return res.data.data;
